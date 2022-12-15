06e86b468cb34972dac751f43f0771648155faec
hokimr <hgyouing@naver.com>
2022-12-15 15:33:16 +0900

    create pags and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signln from "./pages/Signln";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold'>테일윈드</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile " element={<Profile />} />
+          <Route path="/sign-in" element={<Signln />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+
+    </>
+    
+    
   );
 }
 
---
+++ src/pages/Signln.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signln() {
+  return (
+    <div>Signln</div>
+  )
+}
+
+export default Signln
